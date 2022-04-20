a771e6b1ce90479a1c6dd9917ff9e675c033aa94
RayaneKaddoury <rayanekaddoury@gmail.com>
2022-04-20 17:37:54 +0200

    html&css done

---
+++ app.js
@@ -0,0 +1,73 @@
+const collection = [
+    {
+      Model: "M4 F82",
+      Engine: "6 inline turbo twin",
+      ReleaseYear: 2014,
+      Picture: "link/to/a/picture",
+      Chassis: ["Coupe", "Cabriolet"],
+    },
+    {
+      Model: "M4 G82",
+      Engine: "6 inline turbo 3.0",
+      ReleaseYear: 2021,
+      Picture: "link/to/a/picture",
+      Chassis: "Coupe Sport",
+    },
+    {
+      Model: "M3 E30",
+      Engine: "S14",
+      ReleaseYear: 1986,
+      Picture: "link/to/a/picture",
+      Chassis: "Coupe",
+    },
+    {
+      Model: "M3 E46",
+      Engine: "6 inline",
+      ReleaseYear: 2001,
+      Picture: "link/to/a/picture",
+      Chassis: ["Coupe", "Cabriolet"],
+    },
+    {
+      Model: "M3 E90/92",
+      Engine: "V8 4.0L",
+      ReleaseYear: 2007,
+      Picture: "link/to/a/picture",
+      Chassis: ["Coupe", "Cabriolet", "Sedans"],
+    },
+    {
+      Model: "M3 F80",
+      Engine: "6 inline",
+      ReleaseYear: 2014,
+      Picture: "link/to/a/picture",
+      Chassis: "Sedans",
+    },
+    {
+      Model: "M5 E60",
+      Engine: "V10 90°",
+      ReleaseYear: 2005,
+      Picture: "link/to/a/picture",
+      Chassis: ["Sedans", "Touring"],
+    },
+    {
+      Model: "M5 E39",
+      Engine: "V8",
+      ReleaseYear: 1995,
+      Picture: "link/to/a/picture",
+      Chassis: "Berline",
+    },
+    {
+      Model: "M5 E34",
+      Engine: "6 inline 3.8L",
+      ReleaseYear: 1989,
+      Picture: "link/to/a/picture",
+      Chassis: ["Sedans", "Touring"],
+    },
+    {
+      Model: "M1 E26",
+      Engine: "6 inline 24S",
+      ReleaseYear: 2014,
+      Picture: "link/to/a/picture",
+      Chassis: "Coupe",
+    },
+
+  ];
